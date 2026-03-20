434fac78481d963ac4df4ab295c3a7f504fb614e
Adrian <adrianaugustyn24@gmail.com>
2026-03-20 13:52:00 +0100

    sync commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Card } from "@/components/ui/card"
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-row standard-gap flex-wrap">
+    <div className="w-full flex flex-row gap-lg flex-wrap">
       <Card className="small-card"></Card>
       <Card className="large-card"></Card>
       <Card className="small-card"></Card>
